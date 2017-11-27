70c1f5666cab70295d6a80449d84c87025872b13
killian <klanigan@tcd.ie>
2017-11-27 21:12:53 +0000

    Added github access code now prints my github user info to localhost

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 var http  = require('http');
-var fs = require('fs');
+var github = require('octonode');
+var client = github.client('9380b8eadbd961feeaec63fe7fca78797624c644');
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  var myReadStream = fs.createReadStream(__dirname + '/lol.txt', 'utf8');
-  myReadStream.pipe(res);
+  client.get('/user', {}, function (err, status, body, headers) {
+    //console.log(body); //json object
+    res.writeHead(200, {'Content-Type' : 'application/json'});
+    res.end(JSON.stringify(body));
+  });
 });
-
 server.listen(3000, 'localhost');
 console.log('listening');
