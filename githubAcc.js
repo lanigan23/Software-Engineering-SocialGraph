4833135bcca45551f0724809abd150e7596ebf35
killian <klanigan@tcd.ie>
2017-12-09 14:15:02 +0000

    using express to minimise code

--- githubAcc.js
+++ githubAcc.js
@@ -1,13 +1,14 @@
 var http  = require('http');
+var express = require('express');
 var github = require('octonode');
+
+var app = express();
 var client = github.client('9380b8eadbd961feeaec63fe7fca78797624c644');
-var server = http.createServer(function(req, res){
-  console.log('request was made: ' + req.url);
-  client.get('/user', {}, function (err, status, body, headers) {
-    //console.log(body); //json object
-    res.writeHead(200, {'Content-Type' : 'application/json'});
-    res.end(JSON.stringify(body));
+app.get('/', function(req, res){
+  client.get('/user', {}, function (err, status, body, headers){
+    res.send(body);
   });
 });
-server.listen(3000, 'localhost');
+
+app.listen(3000);
 console.log('listening');
