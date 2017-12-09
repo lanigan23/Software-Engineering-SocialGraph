4d619fc561d1bb083bbd7eaf17c51796180cf6f1
killian <klanigan@tcd.ie>
2017-12-09 15:02:07 +0000

    implemented graph with supplied data

--- githubAcc.js
+++ githubAcc.js
@@ -4,9 +4,11 @@ var github = require('octonode');
 
 var app = express();
 var client = github.client('9380b8eadbd961feeaec63fe7fca78797624c644');
+app.use(express.static(__dirname + '/Json'));
 app.get('/', function(req, res){
   client.get('/user', {}, function (err, status, body, headers){
-    res.send(body);
+     //res.send(body);
+    res.sendFile(__dirname + '/index.html');
   });
 });
 
